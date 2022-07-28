bccf7c1be71a737019ce7a0a494b082f37d7d192
GGxu <510374040@qq.com>
2022-07-28 23:21:42 +0800

    set.md.style

--- src/views/menu/pageRead/index.vue
+++ src/views/menu/pageRead/index.vue
@@ -1,44 +1,29 @@
-/*
-* @Description: 文章详情
-* @Author: xuhuazhi
-* @Date: 2022-07-11
-* @LastEditors: xuhuazhi
-* @LastEditTime: 2022-07-11
-*/
 <template>
-  <!-- padding-right: 10px; -->
-  <container :isActive="getHeight" :class="judgment() === 'mobile' ? 'moblie-top-container' : 'moblie-right-container'">
-    <div>
-      {{ reserve.title }}
-    </div>
-    <div>
-      {{ reserve.content }}
-    </div>
-    <img style="hight:400px;width: 500px;" :src=reserve.img alt="">
-  </container>
+  <div class="ueditor">
+    <article class="prose dark:prose-invert">
+        <div v-html=codeData></div>
+    </article>
+    <br>
+  </div>
 </template>
-
-
 <script setup lang="ts">
-import { judgment } from '@/utils/judgment'
 
-
-let getHeight = ref(1)
-const router = useRoute();
-let reserve = ref()
-reserve.value = JSON.parse(sessionStorage.getItem('read'))
-
+let codeData = ref("<p>213asdasdaadsqw<br />\nwqdqwsdasda</p >\n<p>dqwdqwd<br />\n<strong>2132131231</strong></p >\n<h2><a id=\"_5\"></ a>你好啊</h2>\n<p><em>说请问企鹅</em></p >\n")
 
 </script>
+ 
 
-
-
-<style scoped lang="scss" name="read">
-.moblie-top-container {
-  margin-top: 20px;
+<style lang="scss" scoped>
+.hljs {
+  font-size: 15px;
 }
 
-.moblie-right-container {
-  padding-right: 10px;
+.info {
+  border-radius: 10px;
+  line-height: 20px;
+  padding: 10px;
+  margin: 10px;
+  background-color: #ffffff;
 }
 </style>
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   content: ['./src/**/*.{vue,js,ts}'],
-  plugins: [require('daisyui')],
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
   daisyui: {
     themes: ["dark", "garden", "light", "night", "retro", "dracula",],
   },
