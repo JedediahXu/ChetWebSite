17934f6a605122122dc39ff846756299374036ce
GGxu <510374040@qq.com>
2022-08-12 00:28:45 +0800

    修改主题照片

--- src/components/archive/index.vue
+++ src/components/archive/index.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="p-3.5 type-archive">
-    <ul class="medias">
-      <li class="media media-relative" v-for="(src, index) in listPhoto" :intro="src.img" :key="index"
-        @click="toJump(index)">
-        <h3 class="text-overlay">{{ src.name }}</h3>
-        <div data-background-image={{src.cate_photos}} data-loaded="true" class="background loaded"
-          :style="{ backgroundImage: 'url(' + '/apis' + src.cate_photos + ')' }">
+    <div class="not-prose grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 my-10 gap-6">
+      <a class="card border-2 border-base-200 card-compact bg-white/5 hover:bg-gray-300/10 
+        transition-all duration-200 hover:shadow hover:-translate-y-1" v-for="(src, index) in listPhoto"
+        :intro="src.img" :key="index" @click="toJump(index)">
+        <figure class="px-12 pt-6 pb-2 w-full aspect-[2/1] items-end">
+          <img class="w-full h-auto" :src="'/apis' + src.cate_photos" alt="Vite">
+        </figure>
+        <div class="card-body text-center">
+          <span class="text-xs">{{ src.name }}</span>
         </div>
-      </li>
-    </ul>
+      </a>
+    </div>
   </div>
 </template>
 
@@ -23,9 +26,6 @@ const props = defineProps({
     default: true,
   }
 })
-
-console.log(props.listPhoto);
-
 
 const emits = defineEmits(['getTransfer']);
 const toJump = (index) => {
--- src/components/mobile/archive/index.vue
+++ src/components/mobile/archive/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="p-3.5 moblie-type-archive">
-    <ul class="medias">
+  <div class="moblie-type-archive">
+    <!-- <ul class="medias">
       <li class="media media-relative" v-for="(src, index) in listPhoto" :intro="src.img" :key="index"
         @click="toJump(index)">
         <h3 class="text-overlay">{{ src.name }}</h3>
@@ -8,7 +8,19 @@
           :style="{ backgroundImage: 'url(' + '/apis' + src.cate_photos + ')' }">
         </div>
       </li>
-    </ul>
+    </ul> -->
+    <div class="not-prose grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 my-10 gap-6">
+      <a class="card border-2 border-base-200 card-compact bg-white/5 hover:bg-gray-300/10 
+        transition-all duration-200 hover:shadow hover:-translate-y-1" v-for="(src, index) in listPhoto"
+        :intro="src.img" :key="index" @click="toJump(index)">
+        <figure class="px-12 pt-6 pb-2 w-full aspect-[2/1] items-end">
+          <img class="w-full h-auto" :src="'/apis' + src.cate_photos" alt="Vite">
+        </figure>
+        <div class="card-body text-center">
+          <span class="text-xs">{{ src.name }}</span>
+        </div>
+      </a>
+    </div>
   </div>
 </template>
 
