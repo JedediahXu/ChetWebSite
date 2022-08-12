c4f35f9fc76bb97192216d205efaa61d4f48a54e
GGxu <510374040@qq.com>
2022-08-12 08:35:59 +0800

    border-base

--- src/components/archive/index.vue
+++ src/components/archive/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="p-3.5 type-archive">
     <div class="not-prose grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 my-10 gap-6">
-      <a class="card border-2 border-base-200 card-compact bg-white/5 hover:bg-gray-300/10 
+      <a class="card border-2 border-base-100 card-compact bg-white/5 hover:bg-gray-300/10 
         transition-all duration-200 hover:shadow hover:-translate-y-1" v-for="(src, index) in listPhoto"
         :intro="src.img" :key="index" @click="toJump(index)">
         <figure class="px-12 pt-6 pb-2 w-full aspect-[2/1] items-end">
