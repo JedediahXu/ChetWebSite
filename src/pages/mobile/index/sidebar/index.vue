e76d35bf38aa61e3f0937d32e658bb98d8829843
GGxu <510374040@qq.com>
2022-08-18 10:25:01 +0800

    avatar

--- src/pages/mobile/index/sidebar/index.vue
+++ src/pages/mobile/index/sidebar/index.vue
@@ -9,7 +9,7 @@
   <div class="sidebar-top">
     <div class="avatar" style="margin-top: 10px;">
       <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-        <img src="/static/img/avatar/chet.jpg" />
+        <img src="/public/static/img/avatar/xu.jpeg" />
       </div>
     </div>
     <div style="margin-top: 11px;">
