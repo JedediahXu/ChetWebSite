7885f3eefa1e2b1fa50020cde1ac9db4d7f97980
GGxu <510374040@qq.com>
2022-08-19 22:39:41 +0800

    useLoad

---
+++ src/utils/useLoad.ts
@@ -0,0 +1,195 @@
+import { ref } from "vue";
+
+export function useShow() {
+  const listPhotoLoad = ref([{
+    Id: 0,
+  },
+  {
+    Id: 0,
+  }, {
+    Id: 0,
+  }
+    , {
+    Id: 0,
+  },
+  {
+    Id: 0,
+  },
+  {
+    Id: 0,
+  },
+  {
+    Id: 0,
+  },
+  {
+    Id: 0,
+  },
+  {
+    Id: 0,
+  }, {
+    Id: 0,
+  },
+  {
+    Id: 0,
+  },
+  {
+    Id: 0,
+  },
+  {
+    Id: 0,
+  }, {
+    Id: 0,
+  }, {
+    Id: 0,
+  },
+  {
+    Id: 0,
+  },
+  {
+    Id: 0,
+  }, {
+    Id: 0,
+  }, {
+    Id: 0,
+  },
+  {
+    Id: 0,
+  },
+  {
+    Id: 0,
+  }, {
+    Id: 0,
+  }, {
+    Id: 0,
+  },
+  {
+    Id: 0,
+  },
+  {
+    Id: 0,
+  }, {
+    Id: 0,
+  }, {
+    Id: 0,
+  },
+  {
+    Id: 0,
+  },
+  {
+    Id: 0,
+  }, {
+    Id: 0,
+  }])
+  const listArchiveLoad = ref([{
+    Id: 0,
+  },
+  {
+    Id: 0,
+  }, {
+    Id: 0,
+  }
+    , {
+    Id: 0,
+  },
+  {
+    Id: 0,
+  },
+  {
+    Id: 0,
+  },
+  {
+    Id: 0,
+  },
+  {
+    Id: 0,
+  },
+  {
+    Id: 0,
+  }, {
+    Id: 0,
+  }])
+  let listArticleLoad = ref([{
+    Id: 5,
+    author_id: "ChetSerenade",
+    cate_id: 2,
+    content: "文章赵蛟",
+    cover_img: "/uploads/e38127eb7b4c83814ab20a2219eb7ba3",
+    introduce: "一句中心点",
+    is_delete: "0",
+    pub_date: "2022-07-19",
+    state: "已发布",
+    title: "标题",
+  }, {
+    Id: 5,
+    author_id: "ChetSerenade",
+    cate_id: 2,
+    content: "文章赵蛟",
+    cover_img: "/uploads/e38127eb7b4c83814ab20a2219eb7ba3",
+    introduce: "一句中心点",
+    is_delete: "0",
+    pub_date: "2022-07-19",
+    state: "已发布",
+    title: "标题",
+  }, {
+    Id: 5,
+    author_id: "ChetSerenade",
+    cate_id: 2,
+    content: "文章赵蛟",
+    cover_img: "/uploads/e38127eb7b4c83814ab20a2219eb7ba3",
+    introduce: "一句中心点",
+    is_delete: "0",
+    pub_date: "2022-07-19",
+    state: "已发布",
+    title: "标题",
+  }, {
+    Id: 5,
+    author_id: "ChetSerenade",
+    cate_id: 2,
+    content: "文章赵蛟",
+    cover_img: "/uploads/e38127eb7b4c83814ab20a2219eb7ba3",
+    introduce: "一句中心点",
+    is_delete: "0",
+    pub_date: "2022-07-19",
+    state: "已发布",
+    title: "标题",
+  }, {
+    Id: 5,
+    author_id: "ChetSerenade",
+    cate_id: 2,
+    content: "文章赵蛟",
+    cover_img: "/uploads/e38127eb7b4c83814ab20a2219eb7ba3",
+    introduce: "一句中心点",
+    is_delete: "0",
+    pub_date: "2022-07-19",
+    state: "已发布",
+    title: "标题",
+  }, {
+    Id: 5,
+    author_id: "ChetSerenade",
+    cate_id: 2,
+    content: "文章赵蛟",
+    cover_img: "/uploads/e38127eb7b4c83814ab20a2219eb7ba3",
+    introduce: "一句中心点",
+    is_delete: "0",
+    pub_date: "2022-07-19",
+    state: "已发布",
+    title: "标题",
+  }, {
+    Id: 5,
+    author_id: "ChetSerenade",
+    cate_id: 2,
+    content: "文章赵蛟",
+    cover_img: "/uploads/e38127eb7b4c83814ab20a2219eb7ba3",
+    introduce: "一句中心点",
+    is_delete: "0",
+    pub_date: "2022-07-19",
+    state: "已发布",
+    title: "标题",
+  }])
+
+  return {
+    listPhotoLoad,
+    listArchiveLoad,
+    listArticleLoad
+  };
+}
