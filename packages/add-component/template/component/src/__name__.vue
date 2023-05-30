0838bbbb0d5cf720efb01ad42daa748c3d1cf3cd
Chet Xu <510374040@qq.com>
2023-05-30 10:16:15 +0800

    feat:  Generate a vue file

---
+++ packages/add-component/template/component/src/__name__.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup name="<%= displayName %>">
+import { Props } from './props'
+
+const prop = defineProps(Props)
+</script>
+
+<template>
+	<div class="<%= name %>"><%= displayName %></div>
+</template>
